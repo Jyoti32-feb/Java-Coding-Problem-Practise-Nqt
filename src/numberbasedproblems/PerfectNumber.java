1fc37c95f2df0d75208e776ae2165492a411d42b
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-07-01 07:44:42 +0530

    strong number

--- src/numberbasedproblems/PerfectNumber.java
+++ src/numberbasedproblems/PerfectNumber.java
@@ -9,17 +9,37 @@ public class PerfectNumber {
 
         int n = sc.nextInt();
         int sum = 0;
-        for (int i = 1; i < n; i++) {
-            if(n%i==0){
-                sum+=i;
+
+        int originalnumber = n;
+
+        while (n != 0){
+            int rem = n%10;
+            int factorial = 1;
+            for (int i = 1; i <= rem; i++){
+                factorial = factorial * i;
+
             }
+            sum = sum + factorial;
+            n = n/10;
         }
-        if(sum==n){
-            System.out.println("The number is a perfect number.");
+        if (sum == originalnumber){
+            System.out.println("The number is a strong number");
         }
         else{
-            System.out.println("The number is not a perfect number.");
+            System.out.println("The number is not a strong number");
         }
+//        for (int i = 1; i <=n/2; i++) {
+//            if(n%i==0){
+//                sum+=i;
+//            }
+//        }
+//        if(sum==n){
+//            System.out.println("The number is a perfect number.");
+//        }
+//        else{
+//            System.out.println("The number is not a perfect number.");
+//        }
+
     }
 
 }
