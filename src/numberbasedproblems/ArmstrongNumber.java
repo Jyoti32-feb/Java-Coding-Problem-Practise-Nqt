7ee2d9aed1ba4c69cc0af59bb1246d2cb17ac20f
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-07-01 08:16:53 +0530

    Neon Number Added

--- src/numberbasedproblems/ArmstrongNumber.java
+++ src/numberbasedproblems/ArmstrongNumber.java
@@ -7,18 +7,31 @@ public class ArmstrongNumber {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = input.nextInt();
-        int originalNumber = number;
-        int sum = 0;
-        while(number!=0){
-            int rem = number % 10;
-            sum = sum + rem * rem*rem;
-            number = number/10;
+        int squareOfNumber = number*number;
+        int sum=0;
+        while(squareOfNumber!=0){
+            int rem = squareOfNumber%10;
+            sum = sum+rem;
+            squareOfNumber/=10;
         }
-        if(originalNumber==sum){
-            System.out.println("The number is a Armstrong number.");
+        if(sum==number){
+            System.out.println(number+" is neon number");
         }
         else{
-            System.out.println("The number is not a Armstrong number.");
+            System.out.println(number+" is not neon number");
         }
+//        int originalNumber = number;
+//        int sum = 0;
+//        while(number!=0){
+//            int rem = number % 10;
+//            sum = sum + rem * rem*rem;
+//            number = number/10;
+//        }
+//        if(originalNumber==sum){
+//            System.out.println("The number is a Armstrong number.");
+//        }
+//        else{
+//            System.out.println("The number is not a Armstrong number.");
+//        }
     }
 }
