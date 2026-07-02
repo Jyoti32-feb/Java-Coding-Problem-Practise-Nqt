b2eaa358709e8a52fc14b416314a7e4c3e6883f2
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-07-02 09:12:55 +0530

    calculate sum and avrage of an array element

--- src/arraybasedproblems/LargestElement.java
+++ src/arraybasedproblems/LargestElement.java
@@ -1,23 +1,45 @@
 package arraybasedproblems;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class LargestElement {
     static void main(String[] args) {
-        int []numbers={12,21,3,14,51};
+        int []numbers={12,21,3,14,51,11,21,3,4,5};
 
-        int min=numbers[0];
-        for(int num : numbers){
-            if(num<min){
-                min=num;
-            }
-        }
-        System.out.println("the smallest number is :"+min);
+//        int min=numbers[0];
+//        for(int num : numbers){
+//            if(num<min){
+//                min=num;
+//            }
+//        }
+//        System.out.println("the smallest number is :"+min);
 
 //        int max=Integer.MIN_VALUE;
+//        int secmax=Integer.MIN_VALUE;
 //        for(int n:numbers){
 //            if(n>max){
+//                secmax=max;
 //                max=n;
 //            }
 //        }
 //        System.out.println("The largest number is :"+max);
+//        System.out.println("The second largest number is :"+secmax);
+
+//        int sum=0;
+//        for(int i=0;i<numbers.length;i++)
+//        {
+//            sum+=numbers[i];
+//        }
+//        System.out.println("The sum of number is :"+sum);
+//
+//        int avg=sum/numbers.length;
+//        System.out.println("The average of number is :"+avg);
+
+
+
+
     }
 }
---
+++ src/arraybasedproblems/RemoveDuplicate.java
@@ -0,0 +1,4 @@
+package arraybasedproblems;
+
+public class RemoveDuplicate {
+}
