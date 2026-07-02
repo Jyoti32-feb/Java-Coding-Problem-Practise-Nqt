e0681458b828205af963544a17f415f1a24f9209
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-07-02 07:30:28 +0530

    smallest element

--- src/arraybasedproblems/LargestElement.java
+++ src/arraybasedproblems/LargestElement.java
@@ -2,14 +2,22 @@ package arraybasedproblems;
 
 public class LargestElement {
     static void main(String[] args) {
-        int []numbers={1,2,3,4,5};
+        int []numbers={12,21,3,14,51};
 
-        int max=Integer.MIN_VALUE;
-        for(int n:numbers){
-            if(n>max){
-                max=n;
+        int min=numbers[0];
+        for(int num : numbers){
+            if(num<min){
+                min=num;
             }
         }
-        System.out.println("The largest number is :"+max);
+        System.out.println("the smallest number is :"+min);
+
+//        int max=Integer.MIN_VALUE;
+//        for(int n:numbers){
+//            if(n>max){
+//                max=n;
+//            }
+//        }
+//        System.out.println("The largest number is :"+max);
     }
 }
