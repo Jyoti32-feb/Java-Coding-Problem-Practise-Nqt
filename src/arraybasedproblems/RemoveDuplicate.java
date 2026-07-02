9854346e2a56b3507790016044620c9a313f86e4
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-07-02 09:14:35 +0530

    removing duplicates using two pointer , sorting , nested loops and java collection

--- src/arraybasedproblems/RemoveDuplicate.java
+++ src/arraybasedproblems/RemoveDuplicate.java
@@ -1,4 +1,68 @@
 package arraybasedproblems;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class RemoveDuplicate {
+    static void main(String[] args) {
+        int []numbers={12,21,3,14,51,11,21,3,4,5,12,11,14};
+
+//        Set<Integer> set=new HashSet<>();
+//        for(int i=0;i<numbers.length;i++){
+//            set.add(numbers[i]);
+//        }
+//        int size=set.size();
+//        int []numbers2=new int[size];
+//        Iterator<Integer> it=set.iterator();
+//        int i=0;
+//        while(it.hasNext()){
+//            numbers2[i]=it.next();
+//            i++;
+//        }
+//        System.out.println("the unique element in the array are : "+ Arrays.toString(numbers2));
+//
+//        int[] unique = set.stream()
+//                .mapToInt(Integer::intValue)
+//                .toArray();
+
+//        Arrays.sort(numbers);
+//
+//        int i=0;
+//        for(int j=1;j<numbers.length;j++){
+//            if(numbers[i]!=numbers[j]){
+//                i++;
+//                numbers[i]=numbers[j];
+//            }
+//        }
+//        for(int k=0;k<i;k++){
+//            System.out.println(numbers[k]);
+//        }
+//        int index=0;
+//        int uniqueele[]=new int[numbers.length];
+//        for(int i=0;i<numbers.length-1;i++){
+//            if(numbers[i]!=numbers[i+1]){
+//                uniqueele[index++]=numbers[i];
+//            }
+//        }
+//        uniqueele[index++]=numbers[numbers.length-1];
+//        for(int i=0;i<index;i++){
+//            System.out.print(uniqueele[i]+" ");
+//        }
+        //System.out.println("unique elements in array:");
+        System.out.println("Duplicate are :");
+        for(int i=0;i<numbers.length;i++){
+            boolean duplicate=false;
+            for(int j=0;j<i;j++){
+                if(numbers[i]==numbers[j]){
+                    duplicate=true;
+                    break;
+                }
+            }
+            if(duplicate){
+                System.out.print(numbers[i]+"  ");
+            }
+        }
+    }
 }
