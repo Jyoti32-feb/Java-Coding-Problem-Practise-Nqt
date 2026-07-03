b0cf1d3037627bfe0c65a787c828e9923410e03f
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-07-03 08:45:49 +0530

    Emirp Number is added into the file

--- src/numberbasedproblems/PrimeNumber.java
+++ src/numberbasedproblems/PrimeNumber.java
@@ -5,23 +5,59 @@ import java.util.Scanner;
 public class PrimeNumber {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+
+        System.out.print("Enter a number :  " );
+
         int n = sc.nextInt();
-        if(n<=1){
+
+        int original = n;
+
+        int reverseNumber = 0;
+
+        if (isPrime(original)) {
+
+            while (n != 0) {
+
+                int digit = n % 10;
+
+                reverseNumber = reverseNumber * 10 + digit;
+
+                n /= 10;
+            }
+
+            if (isPrime(reverseNumber) && original != reverseNumber) {
+
+                System.out.println("The Number is Emirp Number");
+
+            } else {
+
+                System.out.println("The Number is not Emirp Number");
+
+            }
+
+        } else {
+
+            System.out.println("The Number is not Emirp Number");
+
+        }
+//        if(isPrime){
+//            System.out.println("The number is a prime number.");
+//        }
+//        else{
+//            System.out.println("The number is not a prime number.");
+//        }
+    }
+    public static boolean isPrime(int number){
+        if(number<=1){
             System.out.println("The number is not a prime number.");
         }
         boolean isPrime = true;
-        for (int i = 2; i < Math.sqrt(n); i++){
-            if (n % i == 0){
+        for (int i = 2; i < Math.sqrt(number); i++){
+            if (number % i == 0){
                 isPrime = false;
                 break;
             }
         }
-        if(isPrime){
-            System.out.println("The number is a prime number.");
-        }
-        else{
-            System.out.println("The number is not a prime number.");
-        }
+        return isPrime;
     }
 }
