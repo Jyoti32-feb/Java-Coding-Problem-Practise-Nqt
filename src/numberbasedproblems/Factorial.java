df9902eb43f8dadb5b62c2f065b49f369d712284
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-07-03 07:39:01 +0530

    peterson number is added

--- src/numberbasedproblems/Factorial.java
+++ src/numberbasedproblems/Factorial.java
@@ -6,11 +6,30 @@ public class Factorial {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-        int fact = 1;
-        for (int i = 1; i <= n; i++){
-            fact = fact * i;
+        int num = sc.nextInt();
+        int originalNumber = num;
+
+        int sum = 0;
+        while(num!=0){
+            int digit=num%10;
+            int fact = 1;
+            for (int i = 1; i <= digit; i++){
+                fact = fact * i;
+            }
+            sum = sum + fact;
+            num=num/10;
         }
-        System.out.println("The factorial of "+n+" is "+fact);
+        if(originalNumber==sum){
+            System.out.println("The number is Peterson Number");
+        }
+        else{
+            System.out.println("The number is not Peterson Number");
+        }
+
+
+//        for (int i = 1; i <= n; i++){
+//            fact = fact * i;
+//        }
+//        System.out.println("The factorial of "+n+" is "+fact);
     }
 }
