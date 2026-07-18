be33969d17016aa9b485af9e75ed11661440094b
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-07-18 08:00:37 +0530

    4sum using two pointer approach

--- src/arraybasedproblems/Sum.java
+++ src/arraybasedproblems/Sum.java
@@ -6,37 +6,71 @@ import java.util.List;
 
 public class Sum{
     public static void main(String[] args) {
-        int []arr={-1,0,1,2,-1,-4};
+        int []arr={1,0,-1,0,-2,2};
         Arrays.sort(arr);
         List<List<Integer>> list=new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
-            if(i>0&&arr[i]==arr[i-1]){
+        for(int i=0;i<arr.length-3;i++){
+            if(i>0&&arr[i-1]==arr[i]){
                 continue;
             }
-            int left=i+1;
-            int right=arr.length-1;
-            int target=-arr[i];
-            while(left<right){
-                if(arr[left]+arr[right]==target){
-                    list.add(Arrays.asList(arr[i],arr[left],arr[right]));
-                    right--;
-                    left++;
-
-                    while(left<right&&arr[left]==arr[left+1]){
+            for(int j=i+1;j<arr.length-2;j++){
+                if(j>i+1&&arr[j-1]==arr[j]){
+                    continue;
+                }
+                int left=j+1;
+                int right=arr.length-1;
+                while(left<right){
+                    int sum=arr[i]+arr[j]+arr[left]+arr[right];
+                    if(sum==0){
+                        list.add(Arrays.asList(arr[i],arr[j],arr[left],arr[right]));
+                        while(left<right&&arr[left]==arr[left+1]){
+                            left++;
+                        }
+                        while(left<right&&arr[right]==arr[right-1]){
+                            right--;
+                        }
                         left++;
-                    }
-                    while(left<right&&arr[right]==arr[right-1]){
                         right--;
                     }
-                } else if (arr[left]+arr[right]<target) {
-                    left++;
+                    else if(sum>0){
+                        right--;
+                    }
+                    else{
+                        left++;
+                    }
+                }
 
-                }
-                else {
-                    right--;
-                }
             }
         }
+
+//        for(int i=0;i<arr.length;i++){
+//            if(i>0&&arr[i]==arr[i-1]){
+//                continue;
+//            }
+//            int left=i+1;
+//            int right=arr.length-1;
+//            int target=-arr[i];
+//            while(left<right){
+//                if(arr[left]+arr[right]==target){
+//                    list.add(Arrays.asList(arr[i],arr[left],arr[right]));
+//                    right--;
+//                    left++;
+//
+//                    while(left<right&&arr[left]==arr[left+1]){
+//                        left++;
+//                    }
+//                    while(left<right&&arr[right]==arr[right-1]){
+//                        right--;
+//                    }
+//                } else if (arr[left]+arr[right]<target) {
+//                    left++;
+//
+//                }
+//                else {
+//                    right--;
+//                }
+//            }
+//        }
         System.out.println(list);
     }
 }
